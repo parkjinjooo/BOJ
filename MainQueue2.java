5fccd2c402c3021455a4530f0e19e63bc78ee4c4
parkjinjooo <wlswn2155@gmail.com>
2021-08-14 23:50:13 +0900

    BOJ #1158 요세푸스 문제

---
+++ MainQueue2.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class MainQueue2 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		Queue<Integer> q = new LinkedList<Integer>();
+
+		for (int i = 1; i <= N; i++) {
+			q.offer(i); 
+		}
+		
+		sb.append('<');
+		
+		while (q.size() > 1) {
+			
+			for (int i = 0; i < K-1; i++) {
+				q.offer(q.poll());
+			}
+			
+			sb.append(q.poll()).append(", ");
+
+		}
+		
+		sb.append(q.poll()).append('>');
+		System.out.println(sb);
+		br.close();
+	}
+}
